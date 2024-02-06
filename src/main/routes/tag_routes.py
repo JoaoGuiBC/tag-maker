9464456872e8b067d70ff587bb24827556eb17a9
joaoguibc <joaoguibc@gmail.com>
2024-02-06 13:27:37 -0300

    Implement app in src

---
+++ src/main/routes/tag_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, request, jsonify
+
+tag_routes_bp = Blueprint('tags_routes', __name__)
+
+@tag_routes_bp.route('/create_tag', methods=["POST"])
+def create_tags():
+    print(request.json)
+
+    return jsonify({ "response": "ok" }), 200
